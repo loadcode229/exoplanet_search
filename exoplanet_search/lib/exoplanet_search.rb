5afc51389a2bbcdf1e634fa578edb55306357244
loadcode229 <sepone229@gmail.com>
2020-04-14 12:56:36 -0400

    initial commit

---
+++ exoplanet_search/lib/exoplanet_search.rb
@@ -0,0 +1,7 @@
+#THIS IS YOUR ENVIRONMENT FILE WHERE ALL YOUR CODE GOES
+require "exoplanet_search/version"
+
+module ExoplanetSearch
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ exoplanet_search/lib/exoplanet_search/version.rb
@@ -0,0 +1,3 @@
+module ExoplanetSearch
+  VERSION = "0.1.0"
+end
